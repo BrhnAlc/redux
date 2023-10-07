6987629a16c30f601398723b8601cad62c829593
burhan <alacaburhan122016@gmail.com>
2023-10-07 11:00:22 +0300

    rudex

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const decrement=()=>{
+    setCounter(counter - 1)
+  }
+  const icrement =()=>{
+    setCounter(counter + 1)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span onClick={()=>setCounter(decrement)}>-</span>
+      <span>{counter}</span>
+      <span onClick={()=>setCounter(icrement)}>+</span>
     </div>
   );
 }
